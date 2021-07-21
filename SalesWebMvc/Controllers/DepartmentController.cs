dbf97c3c72c77dba3287d4ec3546e45f0e946ccc
JonathanFRocha <jonathanferreira.contato@gmail.com>
2021-07-21 15:43:26 -0300

    Primeiro controller e view model

---
+++ SalesWebMvc/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Models;
+
+namespace SalesWebMvc.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        public IActionResult Index()
+        {
+            List<Department> deptList = new List<Department>();
+            deptList.Add(new Department { Id = 1, Name = "Eletronics" });
+            deptList.Add(new Department { Id = 2, Name = "Fashion" });
+            return View(deptList);
+        }
+    }
+}
