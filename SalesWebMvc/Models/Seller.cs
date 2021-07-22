ed8ca50d90a48098023343127c7eaf38a261e039
JonathanFRocha <jonathanferreira.contato@gmail.com>
2021-07-22 13:58:03 -0300

    Sales Record Finished

--- SalesWebMvc/Models/Seller.cs
+++ SalesWebMvc/Models/Seller.cs
@@ -9,15 +9,26 @@ namespace SalesWebMvc.Models
     public class Seller
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
+        [StringLength(60, MinimumLength = 3, ErrorMessage = "{0} Name size should be between 3 and 60 {2} and {1}")]
         public string Name { get; set; }
+
+        [EmailAddress(ErrorMessage = "Enter a valid email")]
         [DataType(DataType.EmailAddress)]
+        [Required(ErrorMessage = "{0} required")]
         public string Email { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Birth Date")]
         [DataType(DataType.Date)]
         public DateTime BirthDate { get; set; }
+
+        [Required(ErrorMessage = "{0} required")]
         [Display(Name = "Base Salary")]
-        [DisplayFormat(DataFormatString ="{0:F2}")]
+        [DisplayFormat(DataFormatString = "{0:F2}")]
         public double BaseSalary { get; set; }
+
         public Department Department { get; set; }
         public int DepartmentId { get; set; }
         public ICollection<SalesRecord> Sales { get; set; } = new List<SalesRecord>();
---
+++ SalesWebMvc/Services/Exceptions/IntegrityException.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SalesWebMvc.Services.Exceptions
+{
+    public class IntegrityException : ApplicationException
+    {
+        public IntegrityException(string msg) : base(msg)
+        {
+
+        }
+    }
+}
